9a4e663f51eb9e0716061010c037a0c15c41de39
Olena Mierkulova <mierkulova.tech@gmail.com>
2025-12-03 15:38:23 +0100

    Update subtask.py

--- tasks/serializers/subtask.py
+++ tasks/serializers/subtask.py
@@ -18,15 +18,3 @@ class SubTaskSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'status', 'deadline', 'created_at']
         read_only_fields = ['id', 'created_at']
 
-class SubTaskListCreateView(APIView):
-    def get(self, request):
-        subtasks = SubTask.objects.all()
-        serializer = SubTaskSerializer(subtasks, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = SubTaskCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
