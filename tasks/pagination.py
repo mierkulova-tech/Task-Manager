38c654fc1b210e3d20414e9f2a22820a883282b6
mierkulova-tech <mierkulova.tech@gmail.com>
2025-12-05 17:18:45 +0100

    HW17: add cursor pagination and logging system

--- tasks/pagination.py
+++ tasks/pagination.py
@@ -1,5 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import CursorPagination
 
 
 class SubTaskPagination(PageNumberPagination):
     page_size = 5
+
+
+class SecureCursorPagination(CursorPagination):
+    page_size = 6
+    ordering = '-created_at'
